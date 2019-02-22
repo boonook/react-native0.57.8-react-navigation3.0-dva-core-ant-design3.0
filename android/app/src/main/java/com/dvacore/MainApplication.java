a5e689c3ef5e050ea827b56383a1bb1e9fc8f730
zhuxiuwu <1453855454@qq.com>
2019-02-22 14:04:08 +0800

    缓存清理

--- android/app/src/main/java/com/dvacore/MainApplication.java
+++ android/app/src/main/java/com/dvacore/MainApplication.java
@@ -3,6 +3,7 @@ package com.dvacore;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnta.clear.ClearCachePackage;
 import com.imagepicker.ImagePickerPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ClearCachePackage(),
             new ImagePickerPackage(),
             new RNCameraPackage(),
             new RNSoundPackage(),
